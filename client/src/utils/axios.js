333a05aa025e53e61580a4ac3b22942470e811be
Ssppraveen <135657095+Ssppraveen@users.noreply.github.com>
2025-04-11 11:20:46 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: https://ecommerce-backend-ah99.onrender.com',
+  baseURL: 'https://ecommerce-backend-ah99.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
